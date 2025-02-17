183e005c2d7b11dd35ba3956748eaac4360b6a52
Maciel Zeferino <maciel.zeferino0@gmail.com>
2025-02-17 10:03:18 -0300

    Made the adding of the methods of deleting, updating, searching for id and list

--- src/main/java/com/macielzeferino/financecontrol/service/InvestmentService.java
+++ src/main/java/com/macielzeferino/financecontrol/service/InvestmentService.java
@@ -1,11 +1,14 @@
 package com.macielzeferino.financecontrol.service;
 
 import com.macielzeferino.financecontrol.controller.CreateInvestmentDto;
+import com.macielzeferino.financecontrol.controller.UpdateInvestmentDto;
 import com.macielzeferino.financecontrol.entity.Investment;
 import com.macielzeferino.financecontrol.repository.InvestmentRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -27,6 +30,39 @@ public class InvestmentService {
 
        var investmentSaved = investmentRepository.save(entity);
        return investmentSaved.getInvestmentId();
+    }
+
+    public Optional <Investment> getInvestmentById(String investmentId) {
+        return investmentRepository.findById(UUID.fromString(investmentId));
+    }
+
+    public List<Investment> listInvestments(){
+        return investmentRepository.findAll();
+    }
+
+
+    public void updateInvestmentById(String investmentId, UpdateInvestmentDto updateInvestmentDto) {
+        var id = UUID.fromString(investmentId);
+        var investmentExists = investmentRepository.findById(id);
+        if (investmentExists.isPresent()){
+            var investment = investmentExists.get();
+            if (updateInvestmentDto.investmentName() !=null) {
+                investment.setInvestmentName(updateInvestmentDto.investmentName());
+            }
+            if(updateInvestmentDto.investmentAmount() !=null){
+                investment.setInvestmentAmount(updateInvestmentDto.investmentAmount());
+            }
+            investmentRepository.save(investment);
+        }
+    }
+
+    public void deleteInvestmentById(String investmentId){
+        var id = UUID.fromString(investmentId);
+        var investmentExists = investmentRepository.existsById(id);
+
+        if (investmentExists) {
+            investmentRepository.deleteById(id);
+        }
     }
 
 }
