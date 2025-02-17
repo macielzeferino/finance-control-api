cb9068ecc8fd520044ef8e37209dda2d4714db6a
Maciel Zeferino <maciel.zeferino0@gmail.com>
2025-02-17 09:59:19 -0300

    Addition of deletion, update, list and research routes

--- src/main/java/com/macielzeferino/financecontrol/controller/InvestmentController.java
+++ src/main/java/com/macielzeferino/financecontrol/controller/InvestmentController.java
@@ -3,12 +3,10 @@ package com.macielzeferino.financecontrol.controller;
 import com.macielzeferino.financecontrol.entity.Investment;
 import com.macielzeferino.financecontrol.service.InvestmentService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/investments")
@@ -25,4 +23,32 @@ public class InvestmentController {
         var investmentId = investmentService.createInvestment(createInvestmentDto);
         return  ResponseEntity.created(URI.create("investments/" + investmentId.toString())).build();
     }
+
+    @GetMapping("/{investmentId}")
+    public ResponseEntity<Investment> getInvestmentById(@PathVariable("investmentId")String investmentId){
+
+        var investment = investmentService.getInvestmentById(investmentId);
+        return investment.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Investment>> listInvestments(){
+       var  investments = investmentService.listInvestments();
+       return ResponseEntity.ok(investments);
+    }
+
+    @PutMapping("/{investmentId}")
+     public ResponseEntity<Void> updateInvestmentById(@PathVariable("investmentId")String investmentId,
+                                                 @RequestBody UpdateInvestmentDto updateInvestmentDto){
+        investmentService.updateInvestmentById(investmentId, updateInvestmentDto);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{investmentId}")
+    public ResponseEntity<Void> deleteInvestmentById(@PathVariable("investmentId") String investmentId){
+        investmentService.deleteInvestmentById(investmentId);
+        return ResponseEntity.noContent().build();
+    }
 }
+
+
